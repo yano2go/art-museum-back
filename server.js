964c0a950df99a6ed3099e47217ecf6490ff7f86
JamesLeggero <james.m.leggero@gmail.com>
2020-09-04 16:29:33 -0400

    initial commit back

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const db = mongoose.connection
+const path = require('path')
+const cors = require('cors')
+const PORT = process.env.PORT || 3001
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false
+})
+
+db.on('open', ()=>{
+    console.log('art mongo')
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('Back is connected!')
+})
+
+app.listen(PORT, ()=>{
+    console.log('art back', PORT)
+})
